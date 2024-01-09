c2f9f4c917d3324ae40d97e98cac09b29faeac25
JxstKlaus <janizs.kristof11@gmail.com>
2024-01-09 19:55:43 +0100

    Day 19 part 1 done

---
+++ 2023/Day19/day19_part1.py
@@ -0,0 +1,62 @@
+use_real_data = True
+if use_real_data:
+    file = "day19_input.txt"
+else:
+    file = "day19_example.txt"
+
+with open(f"2023\Day19\{file}","r") as f:
+    data = [line.strip() for line in f]
+    workflows = {}
+    parts = []
+    #workflows, parts = data[:data.index("")],data[data.index("")+1::]
+    for workflow in data[:data.index("")]:
+        name,rules = workflow.split("{")
+        temp = {}
+        rules = rules.split(",")
+        for rule in rules[:-1]:
+            rule = rule.split(":")
+            temp[rule[0]] = rule[1]
+        workflows[name] = temp,rules[-1][:-1]
+
+    
+    for part in data[data.index("")+1::]:
+        part = part[1:-1].split(",")
+        
+        temp = {}
+        for category in part:
+            category = category.split("=")
+            temp[category[0]] = int(category[1])
+        parts.append(temp)
+
+def ruleDecoder(workflow,part): #workflow:  ({'a<2006': 'qkq', 'm>2090': 'A'}, 'rfg'),    part:   {'x': 787, 'm': 2655, 'a': 1222, 's': 2876}
+    for rule in workflow[0]:
+        match rule[1]:
+            case ">":
+                if part[rule[0]] > int(rule[2:]):
+                    return workflow[0][rule]
+            case "<":
+                if part[rule[0]] < int(rule[2:]):
+                    return workflow[0][rule]
+            case "=":
+                if part[rule[0]] == int(rule[2:]):
+                    return workflow[0][rule]
+    return workflow[1]
+
+
+
+total_rating = 0
+for part in parts:
+    workflow_name = "in"
+    while True:
+        if workflow_name == "A":
+            total_rating += sum([part[x] for x in part])
+            break
+        elif workflow_name == "R":
+            break
+        workflow_name = ruleDecoder(workflows[workflow_name],part)
+        
+print(total_rating)
+
+
+
+
